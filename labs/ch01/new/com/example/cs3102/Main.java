2f5956d533ee19224da5e00ac1d78db6ffad12de
Rigel Di Scala <zedr@zedr.com>
2017-11-11 18:29:51 +0000

    Bug fixes

--- labs/ch01/new/com/example/cs3102/Main.java
+++ labs/ch01/new/com/example/cs3102/Main.java
@@ -7,11 +7,25 @@ public class Main {
     public static void main(String args[]) {
         ArrayList<Integer> ints = new ArrayList<>();
         while (ints.size() < 45) {
-            int rnd = (int) ((Math.random() * 100) % 46) + 1;
+            int rnd = (int) ((Math.random() * 100) % 45) + 1;
             if (!ints.contains(rnd)) {
                 ints.add(rnd);
             }
         }
+
+        for (int i = 1; i < 46; i++) {
+            if (!ints.contains(i)) {
+                System.out.println(i + " is not present!");
+            }
+        }
+        if (ints.contains(46)) {
+            System.out.println("46 should not be present!");
+        }
+        if (ints.contains(0)) {
+            System.out.println("0 should not be present!");
+        }
+
+
         System.out.println(ints);
     }
 }
